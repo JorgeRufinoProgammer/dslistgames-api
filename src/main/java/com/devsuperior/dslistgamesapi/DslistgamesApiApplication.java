461e3e39ef53bb4097a8518a7b0dcc8b0030c099
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2023-05-09 17:29:12 -0300

    Projeto criado

---
+++ src/main/java/com/devsuperior/dslistgamesapi/DslistgamesApiApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslistgamesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistgamesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistgamesApiApplication.class, args);
+	}
+
+}
