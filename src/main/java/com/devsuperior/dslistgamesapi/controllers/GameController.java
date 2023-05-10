16baf73061107f879a201b727ab565b11e845e62
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2023-05-10 10:09:37 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslistgamesapi/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslistgamesapi/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.dslistgamesapi.dto.GameDto;
 import com.devsuperior.dslistgamesapi.dto.GameMinDto;
 import com.devsuperior.dslistgamesapi.services.GameService;
 
@@ -22,4 +24,8 @@ public class GameController {
 		return gameService.findAll();
 	}
 
+	@GetMapping("/{id}")
+	public GameDto findById(@PathVariable Long id){
+		return gameService.findById(id);
+	}
 }
--- src/main/java/com/devsuperior/dslistgamesapi/services/GameService.java
+++ src/main/java/com/devsuperior/dslistgamesapi/services/GameService.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslistgamesapi.dto.GameDto;
 import com.devsuperior.dslistgamesapi.dto.GameMinDto;
+import com.devsuperior.dslistgamesapi.entities.Game;
 import com.devsuperior.dslistgamesapi.repositories.GameRepository;
 
 @Service
@@ -13,9 +16,11 @@ public class GameService {
 	
 	@Autowired
 	private GameRepository gameRepository;
+
+//	Assegura que será somente operaçoes de leitura, sem escrita, tornando a operação mais rapida
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDto> findAll(){
-//		Transforma a lista de Games em um lista de GameMinDto
 		List<GameMinDto> dto = gameRepository.findAll().stream()
 				.map(game -> new GameMinDto(game))
 				.toList();
@@ -23,4 +28,10 @@ public class GameService {
 		return dto;
 	}
 
+	@Transactional(readOnly = true)
+	public GameDto findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		
+		return new GameDto(result);
+	}
 }
