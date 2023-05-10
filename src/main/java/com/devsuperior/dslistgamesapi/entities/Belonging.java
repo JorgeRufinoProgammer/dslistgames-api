1a8346540fa3c9efe21e1bec408fc8f64682f728
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2023-05-10 09:28:14 -0300

    Domain model

---
+++ src/main/java/com/devsuperior/dslistgamesapi/entities/Belonging.java
@@ -0,0 +1,64 @@
+package com.devsuperior.dslistgamesapi.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+//Classe responsavel pela posicao dos games na lista
+//A tabela sera composta pela posicao, id de Game, e id de GameList
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+
+	@EmbeddedId
+	private BelongingPK id = new BelongingPK();
+	
+	private Integer position;
+
+	public Belonging() {
+	}
+
+	public Belonging(Game game, GameList gameList, Integer position) {
+		
+		id.setGame(game);
+		id.setGameList(gameList);
+		this.position = position;
+	}
+
+	public BelongingPK getId() {
+		return id;
+	}
+
+	public void setId(BelongingPK id) {
+		this.id = id;
+	}
+
+	public Integer getPosition() {
+		return position;
+	}
+
+	public void setPosition(Integer position) {
+		this.position = position;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Belonging other = (Belonging) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
