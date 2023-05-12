777ac3c85179213a405a3747da803448b1a61d07
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2023-05-12 19:54:26 -0300

    Move games on list

--- src/main/java/com/devsuperior/dslistgamesapi/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslistgamesapi/controllers/GameListController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,5 +38,11 @@ public class GameListController {
 	@GetMapping("/{listId}/games")
 	public List<GameMinDto> findByList(@PathVariable Long listId){
 		return gameService.findByList(listId);
+	}
+	
+	@PostMapping("/{listId}/replacement")
+	public void move(@PathVariable Long listId, @RequestBody ReplacementDto replacementDto){
+		
+		gameListService.move(listId, replacementDto.getSourceIndex(), replacementDto.getDestinationIndex());		
 	}
 }
--- src/main/java/com/devsuperior/dslistgamesapi/services/GameListService.java
+++ src/main/java/com/devsuperior/dslistgamesapi/services/GameListService.java
@@ -8,13 +8,18 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslistgamesapi.dto.GameListDto;
 import com.devsuperior.dslistgamesapi.entities.GameList;
+import com.devsuperior.dslistgamesapi.projections.GameMinProjection;
 import com.devsuperior.dslistgamesapi.repositories.GameListRepository;
+import com.devsuperior.dslistgamesapi.repositories.GameRepository;
 
 @Service
 public class GameListService {
 	
 	@Autowired
 	private GameListRepository gameListRepository;
+	
+	@Autowired
+	private GameRepository gameRepository;
 
 //	Assegura que será somente operaçoes de leitura, sem escrita, tornando a operação mais rapida
 	
@@ -32,5 +37,23 @@ public class GameListService {
 		GameList result = gameListRepository.findById(id).get();
 		
 		return new GameListDto(result);
+	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		
+//		Se o index de origem for menor que o index de destino, o menor é a origem, se não é o destino
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		
+//		O contrario
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for(int i = min ; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
 	}
 }
